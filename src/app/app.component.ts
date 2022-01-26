95d067d04eeab56e5244f197fcf3f9986d6bd6a3
amilineniraghu <raghu.amilineni@gmail.com>
2022-01-26 10:23:52 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit{
   name = 'Angular ' + VERSION.major;
+  public testForm:FormGroup;
+  constructor(private formbuilder: FormBuilder){
+    
+  }
+
+ngOnInit(): void {
+  this.createForm();
+}
+
+createForm() {
+
+  this.testForm = this.formbuilder.group(
+    {
+      date : new FormControl('')
+    }
+  );   
+}
+
+onSubmit(){
+  console.info(this.testForm.value);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CalendarModule} from 'primeng/calendar';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule,CalendarModule,BrowserAnimationsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
